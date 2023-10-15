2f9deedf06b489d13f007b9c9ec0ee9919d69c47
Hisham Al-Eesa <hisham.aleesa@gmail.com>
2023-10-15 15:54:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+
+const configurationObject = {
+    method : "POST",
+    headers: {
+        "Content-Type":"application/json",
+        Accept: "application/json"
+    },
+    body: JSON.stringify(formData)
+};
+
+function submitData(){
+  const body = document.querySelector('body');
+  return fetch("http://localhost:3000/users",configurationObject)
+  .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      body.append(object.id);
+    }) 
+    .catch(function (error) {
+      body.append(error.message)
+    });
+}
